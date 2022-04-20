a2466f4ea9d5ede6f5816679be5e79187b971ccb
jimbob-git <40457714@live.napier.ac.uk>
2022-04-20 15:49:00 +0100

    .js file updated with all questions and answers

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const Questions = [{
     {
         id: 1,
         q: "Q2. London has six major airports, 5 of which are: Gatwick, Heathrow, Luton, Southend and Stanstead, name the sixth one?",
-        a: [{ text: "London Palace", isCorrect: false, isSelected: false },
+        a: [{ text: "London Palace", isCorrect: false },
             { text: "London House", isCorrect: false },
             { text: "London North Castle", isCorrect: false },
             { text: "London City", isCorrect: true }
@@ -31,111 +31,101 @@ const Questions = [{
     },
 	{
         id: 3,
-        q: "Q2. Q4. Singaporeans speak a special dialect of English which is known as what?",
-        a: [{ text: "Singlish", isCorrect: false, isSelected: false },
+        q: "Q4. Singaporeans speak a special dialect of English which is known as what?",
+        a: [{ text: "Singlish", isCorrect: true },
             { text: "Tamil", isCorrect: false },
             { text: "Cantonese", isCorrect: false },
-            { text: "Mandarin", isCorrect: true }
+            { text: "Mandarin", isCorrect: false }
         ]
   
     },
     {
         id: 4,
-        q: "Q3. What is the name of the worlds biggest movie industry?",
-        a: [{ text: "Hollywood", isCorrect: false },
-            { text: "Mollywood", isCorrect: false },
-            { text: "Bollywood", isCorrect: true },
-            { text: "Malayalam", isCorrect: false }
+        q: "Q5. Bangkok is the capital city of which country?",
+        a: [{ text: "Vietnam", isCorrect: false },
+            { text: "Malaysia", isCorrect: false },
+            { text: "Burma", isCorrect: false },
+            { text: "Thailand", isCorrect: true }
         ]
   
     },
 	{
         id: 5,
-        q: "Q2. London has six major airports, 5 of which are: Gatwick, Heathrow, Luton, Southend and Stanstead, name the sixth one?",
-        a: [{ text: "London Palace", isCorrect: false, isSelected: false },
-            { text: "London House", isCorrect: false },
-            { text: "London North Castle", isCorrect: false },
-            { text: "London City", isCorrect: true }
+        q: "Q6. Phuket was hit by what on the 26th December 2004?",
+        a: [{ text: "An earthquake", isCorrect: false },
+            { text: "A hurricane", isCorrect: false },
+            { text: "A tsunami", isCorrect: true },
+            { text: "A volcanic eruption", isCorrect: false }
         ]
   
     },
     {
         id: 6,
-        q: "Q3. What is the name of the worlds biggest movie industry?",
-        a: [{ text: "Hollywood", isCorrect: false },
-            { text: "Mollywood", isCorrect: false },
-            { text: "Bollywood", isCorrect: true },
-            { text: "Malayalam", isCorrect: false }
+        q: "Q7. What is the name of the Sydney bridge in the picture?",
+        a: [{ text: "Balmain Bridge", isCorrect: false },
+            { text: "Anzac Bridge", isCorrect: true },
+            { text: "Sydney Harbour Bridge", isCorrect: false },
+            { text: "Bondi Bridge", isCorrect: false }
         ]
   
     },
 	{
         id: 7,
-        q: "Q2. London has six major airports, 5 of which are: Gatwick, Heathrow, Luton, Southend and Stanstead, name the sixth one?",
-        a: [{ text: "London Palace", isCorrect: false, isSelected: false },
-            { text: "London House", isCorrect: false },
-            { text: "London North Castle", isCorrect: false },
-            { text: "London City", isCorrect: true }
+        q: "Q8. Melbourne is home to the Australian Formula One Grand Prix and what other international sporting event?",
+        a: [{ text: "The Australian Open", isCorrect: true },
+            { text: "The Oceania Rugby Tournament", isCorrect: false },
+            { text: "The Australian Rules Football Final", isCorrect: false },
+            { text: "South Australian Kangaroo Race", isCorrect: false }
         ]
   
     },
     {
         id: 8,
-        q: "Q3. What is the name of the worlds biggest movie industry?",
-        a: [{ text: "Hollywood", isCorrect: false },
-            { text: "Mollywood", isCorrect: false },
-            { text: "Bollywood", isCorrect: true },
-            { text: "Malayalam", isCorrect: false }
+        q: "Q9. What is the capital city of New Zealand's South Island?",
+        a: [{ text: "Christchurch", isCorrect: true },
+            { text: "Wellington", isCorrect: false },
+            { text: "Queenstown", isCorrect: false },
+            { text: "Auckland", isCorrect: false }
         ]
   
     },
 	{
         id: 9,
-        q: "Q2. London has six major airports, 5 of which are: Gatwick, Heathrow, Luton, Southend and Stanstead, name the sixth one?",
-        a: [{ text: "London Palace", isCorrect: false, isSelected: false },
-            { text: "London House", isCorrect: false },
-            { text: "London North Castle", isCorrect: false },
-            { text: "London City", isCorrect: true }
+        q: "Q10. Fiji sits in which Ocean?",
+        a: [{ text: "Atlantic Ocean", isCorrect: false },
+            { text: "South Pacific Ocean", isCorrect: true },
+            { text: "Indian Ocean", isCorrect: false },
+            { text: "Arctic Ocean", isCorrect: false }
         ]
   
     },
     {
         id: 10,
-        q: "Q3. What is the name of the worlds biggest movie industry?",
-        a: [{ text: "Hollywood", isCorrect: false },
-            { text: "Mollywood", isCorrect: false },
-            { text: "Bollywood", isCorrect: true },
-            { text: "Malayalam", isCorrect: false }
+        q: "Q11. Rarotonga is the largest island in the southern group of what islands?",
+        a: [{ text: "Cook", isCorrect: true },
+            { text: "Queen", isCorrect: false },
+            { text: "Bahamas", isCorrect: false },
+            { text: "Sandwich", isCorrect: false }
         ]
   
     },
 	{
         id: 11,
-        q: "Q2. London has six major airports, 5 of which are: Gatwick, Heathrow, Luton, Southend and Stanstead, name the sixth one?",
-        a: [{ text: "London Palace", isCorrect: false, isSelected: false },
-            { text: "London House", isCorrect: false },
-            { text: "London North Castle", isCorrect: false },
-            { text: "London City", isCorrect: true }
+        q: "Q12. The Hollywood sign in Los Angeles was originally built in 1923, however it had a longer name and was changed to Hollywood in 1949, what was the original name?",
+        a: [{ text: "Hollywood Town", isCorrect: false },
+            { text: "Movie Town", isCorrect: false },
+            { text: "HollywoodMovieLand", isCorrect: false },
+            { text: "HollywoodLand", isCorrect: true }
         ]
   
     },
     {
         id: 12,
-        q: "Q3. What is the name of the worlds biggest movie industry?",
-        a: [{ text: "Hollywood", isCorrect: false },
-            { text: "Mollywood", isCorrect: false },
-            { text: "Bollywood", isCorrect: true },
-            { text: "Malayalam", isCorrect: false }
-        ]
-  
-    },
-	{
-        id: 13,
-        q: "Q2. London has six major airports, 5 of which are: Gatwick, Heathrow, Luton, Southend and Stanstead, name the sixth one?",
-        a: [{ text: "London Palace", isCorrect: false, isSelected: false },
-            { text: "London House", isCorrect: false },
-            { text: "London North Castle", isCorrect: false },
-            { text: "London City", isCorrect: true }
+        q: "Q13. What year was the great earthquake that shook San Francisco, measuring 7.9 on the richter scale?",
+        a: [{ text: "1901", isCorrect: false },
+            { text: "1906", isCorrect: true },
+            { text: "1926", isCorrect: false },
+            { text: "1856", isCorrect: false }
         ]
   
     }
